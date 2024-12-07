f9567fef77bc6735116853bf876c41c6ba7859a0
Manoel Amaro <manoel.al.amaro@gmail.com>
2024-12-07 10:10:38 +0100

    2024 07

---
+++ cmd/aoc2024/day7/main.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"strings"
+
+	"manoamaro.github.com/advent-of-code/pkg/aoc"
+	"manoamaro.github.com/advent-of-code/pkg/strings2"
+)
+
+type INPUT [][]int
+type RESULT int
+
+var challenge = aoc.New(2024, 7, parseInput, part1, part2)
+
+func parseInput(input string) (INPUT, error) {
+	input = strings.TrimSpace(input)
+	lines := strings.Split(input, "\n")
+	o := make([][]int, len(lines))
+	for i, line := range lines {
+		parts := strings.Split(line, ":")
+		v := strings2.Atoi[int](parts[0])
+		v2 := strings2.MapToInt(strings.Fields(parts[1]))
+		eq := make([]int, 1)
+		eq[0] = v
+		eq = append(eq, v2...)
+		o[i] = eq
+	}
+	return o, nil
+}
+
+func calculatePart1(expected int, curr int, nums []int) bool {
+	if len(nums) == 0 {
+		return expected == curr
+	}
+	return calculatePart1(expected, curr+nums[0], nums[1:]) || calculatePart1(expected, curr*nums[0], nums[1:])
+}
+
+func part1(input INPUT) (RESULT, error) {
+	total := 0
+	for _, eq := range input {
+		if calculatePart1(eq[0], eq[1], eq[2:]) {
+			total += eq[0]
+		}
+	}
+	return RESULT(total), nil
+}
+
+// concat: Concatenates two integers e.g. concat(1,2) = 12
+func concat(a, b int) int {
+	p := 1
+	for p <= b {
+		p *= 10
+	}
+	return a*p + b
+}
+
+func calculatePart2(expected int, curr int, nums []int) bool {
+	if len(nums) == 0 {
+		return expected == curr
+	}
+	return calculatePart2(expected, curr+nums[0], nums[1:]) || calculatePart2(expected, curr*nums[0], nums[1:]) || calculatePart2(expected, concat(curr, nums[0]), nums[1:])
+}
+
+func part2(input INPUT) (RESULT, error) {
+	total := 0
+	for _, eq := range input {
+		if calculatePart2(eq[0], eq[1], eq[2:]) {
+			total += eq[0]
+		}
+	}
+	return RESULT(total), nil
+}
+
+func main() {
+	challenge.Run()
+}
